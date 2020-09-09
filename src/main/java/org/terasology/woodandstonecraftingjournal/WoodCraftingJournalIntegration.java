1754a7e8309f16883d4a7e355992a60c77d01c62
DarkWeird <foxflameless@gmail.com>
2020-09-09 17:06:41 +0300

    feature(reintegrate-gestaltv7):  gestalt-v7 compability (package)

--- src/main/java/org/terasology/woodandstonecraftingjournal/WoodCraftingJournalIntegration.java
+++ src/main/java/org/terasology/woodandstonecraftingjournal/WoodCraftingJournalIntegration.java
@@ -1,28 +1,25 @@
-/*
- * Copyright 2017 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.woodandstonecraftingjournal;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.terasology.entitySystem.entity.EntityRef;
-import org.terasology.entitySystem.event.ReceiveEvent;
-import org.terasology.entitySystem.prefab.Prefab;
-import org.terasology.entitySystem.prefab.PrefabManager;
-import org.terasology.entitySystem.systems.BaseComponentSystem;
-import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.engine.entitySystem.entity.EntityRef;
+import org.terasology.engine.entitySystem.event.ReceiveEvent;
+import org.terasology.engine.entitySystem.prefab.Prefab;
+import org.terasology.engine.entitySystem.prefab.PrefabManager;
+import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
+import org.terasology.engine.entitySystem.systems.RegisterSystem;
+import org.terasology.engine.logic.characters.CharacterComponent;
+import org.terasology.engine.logic.players.event.OnPlayerSpawnedEvent;
+import org.terasology.engine.registry.In;
+import org.terasology.engine.rendering.nui.widgets.browser.data.ParagraphData;
+import org.terasology.engine.rendering.nui.widgets.browser.data.basic.HTMLLikeParser;
+import org.terasology.engine.rendering.nui.widgets.browser.ui.style.ParagraphRenderStyle;
+import org.terasology.engine.utilities.Assets;
+import org.terasology.engine.world.block.Block;
+import org.terasology.engine.world.block.BlockManager;
+import org.terasology.inventory.logic.events.InventorySlotChangedEvent;
 import org.terasology.journal.BrowserJournalChapterHandler;
 import org.terasology.journal.DiscoveredNewJournalEntry;
 import org.terasology.journal.JournalEntryProducer;
@@ -30,23 +27,13 @@ import org.terasology.journal.JournalManager;
 import org.terasology.journal.TimestampResolver;
 import org.terasology.journal.ui.ImageParagraph;
 import org.terasology.journal.ui.RecipeParagraph;
-import org.terasology.logic.characters.CharacterComponent;
-import org.terasology.logic.inventory.events.InventorySlotChangedEvent;
-import org.terasology.logic.players.event.OnPlayerSpawnedEvent;
 import org.terasology.multiBlock.MultiBlockFormed;
 import org.terasology.nui.HorizontalAlign;
-import org.terasology.registry.In;
-import org.terasology.rendering.nui.widgets.browser.data.ParagraphData;
-import org.terasology.rendering.nui.widgets.browser.data.basic.HTMLLikeParser;
-import org.terasology.rendering.nui.widgets.browser.ui.style.ParagraphRenderStyle;
-import org.terasology.utilities.Assets;
 import org.terasology.workstationCrafting.component.CraftInHandIngredientComponent;
 import org.terasology.workstationCrafting.component.CraftingStationComponent;
 import org.terasology.workstationCrafting.component.CraftingStationIngredientComponent;
 import org.terasology.workstationCrafting.component.CraftingStationToolComponent;
 import org.terasology.workstationCrafting.event.CraftingStationUpgraded;
-import org.terasology.world.block.Block;
-import org.terasology.world.block.BlockManager;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -54,23 +41,21 @@ import java.util.List;
 
 @RegisterSystem
 public class WoodCraftingJournalIntegration extends BaseComponentSystem {
+    private static final Logger logger = LoggerFactory.getLogger(WoodCraftingJournalIntegration.class);
+    private final String wasChapterId = "WoodAndStoneCrafting";
+    private final String seasonsChapterId = "Seasons";
+    private final ParagraphRenderStyle centerRenderStyle = new ParagraphRenderStyle() {
+        @Override
+        public HorizontalAlign getHorizontalAlignment() {
+            return HorizontalAlign.CENTER;
+        }
+    };
     @In
     private JournalManager journalManager;
     @In
     private PrefabManager prefabManager;
     @In
     private BlockManager blockManager;
-
-    private static final Logger logger = LoggerFactory.getLogger(WoodCraftingJournalIntegration.class);
-    private String wasChapterId = "WoodAndStoneCrafting";
-    private String seasonsChapterId = "Seasons";
-
-    private ParagraphRenderStyle centerRenderStyle = new ParagraphRenderStyle() {
-        @Override
-        public HorizontalAlign getHorizontalAlignment() {
-            return HorizontalAlign.CENTER;
-        }
-    };
 
     @Override
     public void preBegin() {
@@ -95,7 +80,8 @@ public class WoodCraftingJournalIntegration extends BaseComponentSystem {
                 Arrays.asList(
                         createTitleParagraph("Wood and Stone"),
                         createSubTitleParagraph("Stones"),
-                        createTextParagraph("Stones are essential for making most tools. Digging the ground might be a good way to find stones." +
+                        createTextParagraph("Stones are essential for making most tools. Digging the ground might be " +
+                                "a good way to find stones." +
                                 "Once I get two stones, I should be able to make a Tool Stone."),
                         new ImageParagraph(new Prefab[]{stoneItem}, null)
                 ));
@@ -103,56 +89,74 @@ public class WoodCraftingJournalIntegration extends BaseComponentSystem {
         chapterHandler.registerJournalEntry("ToolStone",
                 Arrays.asList(
                         createSubTitleParagraph("Tool Stone"),
-                        createTextParagraph("Now that I have stones, I can use two stones to make a Tool Stone which will allow me to build other equipment like an axe " +
+                        createTextParagraph("Now that I have stones, I can use two stones to make a Tool Stone which " +
+                                "will allow me to build other equipment like an axe " +
                                 "(press N to open crafting window)."),
                         new ImageParagraph(new Prefab[]{toolStoneItem}, null),
-                        createTextParagraph("By using the Tool Stone on another stone I should be able to make an Axe-Hammer Head."),
-                        new RecipeParagraph(new Block[2], new Prefab[]{toolStoneItem, stoneItem}, null, axeHammerHeadItem, 1)
+                        createTextParagraph("By using the Tool Stone on another stone I should be able to make an " +
+                                "Axe-Hammer Head."),
+                        new RecipeParagraph(new Block[2], new Prefab[]{toolStoneItem, stoneItem}, null,
+                                axeHammerHeadItem, 1)
                 ));
 
         chapterHandler.registerJournalEntry("AxeHammer",
                 Arrays.asList(
                         createSubTitleParagraph("Axe-Hammer"),
-                        createTextParagraph("Now that I have an axe-hammer head, all I need is a stick for the handle and a twig to tie it together. " +
+                        createTextParagraph("Now that I have an axe-hammer head, all I need is a stick for the handle" +
+                                " and a twig to tie it together. " +
                                 "I should be able to find these in the branches of trees."),
                         new ImageParagraph(new Prefab[]{stickItem, twigItem}, null),
-                        createTextParagraph("Then I can combine the Axe-Hammer Head with a Stick and a Twig to create a Crude Axe-Hammer."),
-                        new RecipeParagraph(new Block[3], new Prefab[]{axeHammerHeadItem, stickItem, twigItem}, null, crudeAxeHammerItem, 1)
+                        createTextParagraph("Then I can combine the Axe-Hammer Head with a Stick and a Twig to create" +
+                                " a Crude Axe-Hammer."),
+                        new RecipeParagraph(new Block[3], new Prefab[]{axeHammerHeadItem, stickItem, twigItem}, null,
+                                crudeAxeHammerItem, 1)
                 ));
 
         chapterHandler.registerJournalEntry("AxeHammerReceived",
                 Arrays.asList(
                         createSubTitleParagraph("Axe-Hammer Created"),
-                        createTextParagraph(("Excellent! I got the Axe-Hammer. I should be able to cut some of the trees with it. " +
-                        "I can also use it to dig stone to get some more Stones for my crafting. It's not perfect but will have to do until I get my hands on a " +
-                        "better hammer or a pick."))
+                        createTextParagraph(("Excellent! I got the Axe-Hammer. I should be able to cut some of the " +
+                                "trees with it. " +
+                                "I can also use it to dig stone to get some more Stones for my crafting. It's not " +
+                                "perfect but will have to do until I get my hands on a " +
+                                "better hammer or a pick."))
                 ));
 
         chapterHandler.registerJournalEntry("Logs",
                 Arrays.asList(
                         createSubTitleParagraph("Logs"),
                         createTextParagraph(("These logs are big, there is no way I can handle them with my hands. " +
-                        "I have to build a place where I can work on them. I should place two of the logs on the ground next to each other " +
-                        "and then place my Axe on it (right-click your Axe-Hammer on the top face of one of the logs)."))
+                                "I have to build a place where I can work on them. I should place two of the logs on " +
+                                "the ground next to each other " +
+                                "and then place my Axe on it (right-click your Axe-Hammer on the top face of one of " +
+                                "the logs)."))
                 ));
 
         chapterHandler.registerJournalEntry("WoodCraftingStation",
                 Arrays.asList(
                         createSubTitleParagraph("Basic Wood Crafting Station"),
-                        createTextParagraph(("Now I can work on the logs (to open the interface, press 'E' while pointing " +
-                        "on the station).<l><l>I can store some ingredients in the top-left corner of the station and my axe in the bottom-center " +
-                        "of the station. It's very crude and won't let me do much, but once I gather 10 Wood Planks I should be able to upgrade it. " +
-                        "(to upgrade place the ingredients into bottom-left corner of the interface and press the 'Upgrade' button)"))
+                        createTextParagraph(("Now I can work on the logs (to open the interface, press 'E' while " +
+                                "pointing " +
+                                "on the station).<l><l>I can store some ingredients in the top-left corner of the " +
+                                "station and my axe in the bottom-center " +
+                                "of the station. It's very crude and won't let me do much, but once I gather 10 Wood " +
+                                "Planks I should be able to upgrade it. " +
+                                "(to upgrade place the ingredients into bottom-left corner of the interface and press" +
+                                " the 'Upgrade' button)"))
                 ));
 
         chapterHandler.registerJournalEntry("WoodCraftingStationUpgraded",
                 Arrays.asList(
                         createSubTitleParagraph("Standard Wood Crafting Station"),
                         createTextParagraph(("Finally I can make something more useful than just planks. " +
-                        "Not only that, but I can also create planks more efficiently! Quality of the workspace speaks for itself.<l><l>" +
-                        "But I still can't make any tools. Hard to make it just out of wood, haha. I should probably find a good place " +
-                        "to work on stone materials. I should make two tables using planks and sticks.<l><l>Once I get the tables I should place them " +
-                        "on the ground next to each other and put my Axe-Hammer on top of one of them (same as before)."))
+                                "Not only that, but I can also create planks more efficiently! Quality of the " +
+                                "workspace speaks for itself.<l><l>" +
+                                "But I still can't make any tools. Hard to make it just out of wood, haha. I should " +
+                                "probably find a good place " +
+                                "to work on stone materials. I should make two tables using planks and sticks" +
+                                ".<l><l>Once I get the tables I should place them " +
+                                "on the ground next to each other and put my Axe-Hammer on top of one of them (same " +
+                                "as before)."))
                 ));
 
         chapterHandler.registerJournalEntry("StoneCraftingStation",
@@ -160,18 +164,27 @@ public class WoodCraftingJournalIntegration extends BaseComponentSystem {
                     @Override
                     public Collection<ParagraphData> produceParagraph(long date) {
                         return Arrays.asList(
-                                HTMLLikeParser.parseHTMLLikeParagraph(centerRenderStyle, TimestampResolver.getJournalEntryDate(date)),
+                                HTMLLikeParser.parseHTMLLikeParagraph(centerRenderStyle,
+                                        TimestampResolver.getJournalEntryDate(date)),
                                 createSubTitleParagraph("Basic Stone Crafting Station"),
-                                createTextParagraph("Now! On this workstation I should be able to create more durable tools. " +
+                                createTextParagraph("Now! On this workstation I should be able to create more durable" +
+                                        " tools. " +
                                         "I should get myself a couple of hammers and finally go mining!"),
-                                new RecipeParagraph(new Block[3], new Prefab[]{stoneItem, twigItem, stickItem}, null, stoneHammerItem, 1),
-                                createTextParagraph("It is going to be dark out there in the mines, I should prepare some torches in advance. " +
-                                        "I can use some of the Resin found while cutting trees with stick in a crafting window (press G) to " +
+                                new RecipeParagraph(new Block[3], new Prefab[]{stoneItem, twigItem, stickItem}, null,
+                                        stoneHammerItem, 1),
+                                createTextParagraph("It is going to be dark out there in the mines, I should prepare " +
+                                        "some torches in advance. " +
+                                        "I can use some of the Resin found while cutting trees with stick in a " +
+                                        "crafting window (press G) to " +
                                         "create Unlit Torches."),
-                                new RecipeParagraph(new Block[2], new Prefab[]{resinItem, stickItem}, null, unlitTorchItem, 1),
-                                createTextParagraph("Once I get them I should be able to light them up using flint in a crafting window. " +
-                                        "Just need to make sure not to light too many of them, as the torches last only for a bit of time."),
-                                new RecipeParagraph(new Block[2], new Prefab[]{unlitTorchItem, flintItem}, litTorchBlock, null, 1)
+                                new RecipeParagraph(new Block[2], new Prefab[]{resinItem, stickItem}, null,
+                                        unlitTorchItem, 1),
+                                createTextParagraph("Once I get them I should be able to light them up using flint in" +
+                                        " a crafting window. " +
+                                        "Just need to make sure not to light too many of them, as the torches last " +
+                                        "only for a bit of time."),
+                                new RecipeParagraph(new Block[2], new Prefab[]{unlitTorchItem, flintItem},
+                                        litTorchBlock, null, 1)
                         );
                     }
                 });
@@ -186,7 +199,8 @@ public class WoodCraftingJournalIntegration extends BaseComponentSystem {
             @Override
             public Collection<ParagraphData> produceParagraph(long date) {
                 return Arrays.asList(
-                        HTMLLikeParser.parseHTMLLikeParagraph(centerRenderStyle, TimestampResolver.getJournalEntryDate(date)),
+                        HTMLLikeParser.parseHTMLLikeParagraph(centerRenderStyle,
+                                TimestampResolver.getJournalEntryDate(date)),
                         HTMLLikeParser.parseHTMLLikeParagraph(null,
                                 text));
             }
@@ -198,7 +212,8 @@ public class WoodCraftingJournalIntegration extends BaseComponentSystem {
     }
 
     private ParagraphData createTitleParagraph(String title) {
-        return HTMLLikeParser.parseHTMLLikeParagraph(centerRenderStyle, "<f engine:NotoSans-Regular-Title>" + title + "</f>");
+        return HTMLLikeParser.parseHTMLLikeParagraph(centerRenderStyle,
+                "<f engine:NotoSans-Regular-Title>" + title + "</f>");
     }
 
     private ParagraphData createSubTitleParagraph(String title) {
@@ -233,9 +248,12 @@ public class WoodCraftingJournalIntegration extends BaseComponentSystem {
     @ReceiveEvent
     public void playerPickedUpItem(InventorySlotChangedEvent event, EntityRef character,
                                    CharacterComponent characterComponent) {
-        CraftingStationToolComponent toolComponent = event.getNewItem().getComponent(CraftingStationToolComponent.class);
-        CraftingStationIngredientComponent ingredientComponent = event.getNewItem().getComponent(CraftingStationIngredientComponent.class);
-        CraftInHandIngredientComponent craftInHandIngredientComponent = event.getNewItem().getComponent(CraftInHandIngredientComponent.class);
+        CraftingStationToolComponent toolComponent =
+                event.getNewItem().getComponent(CraftingStationToolComponent.class);
+        CraftingStationIngredientComponent ingredientComponent =
+                event.getNewItem().getComponent(CraftingStationIngredientComponent.class);
+        CraftInHandIngredientComponent craftInHandIngredientComponent =
+                event.getNewItem().getComponent(CraftInHandIngredientComponent.class);
         if (toolComponent != null) {
             List<String> toolTypes = toolComponent.type;
             if (toolTypes.contains("hammer") && toolTypes.contains("axe")) {
